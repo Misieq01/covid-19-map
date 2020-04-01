86bb7f45d5a176b76f2889d31211574e4beff24c
Misieq01 <38589417+Misieq01@users.noreply.github.com>
2020-04-01 16:59:55 +0200

    :cactus:  Retouched search bar + added clear option to it

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,16 +1,32 @@
 import React from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { searchCountry } from "../store/actions";
+import { getSearchValue } from "../store/selectors";
+import { ReactComponent as SearchIcon } from "../assets/search.svg";
+import { ReactComponent as ClearIcon } from "../assets/clear-search.svg";
 
 const SearchBar = () => {
   const dispatch = useDispatch();
-
+  const searchValue = useSelector(state => getSearchValue(state));
   return (
-    <input
-      className="search-bar__input"
-      placeholder="Search for country"
-      onChange={event => dispatch(searchCountry(event.target.value))}
-    />
+    <div className="search-bar__wrapper">
+      <input
+        value={searchValue}
+        className="search-bar__input"
+        placeholder="Search for country"
+        onChange={event => dispatch(searchCountry(event.target.value))}
+      />
+      {searchValue ? (
+        <ClearIcon
+          className="search-bar__icon--hover"
+          title="Clear"
+          onClick={() => dispatch(searchCountry(""))}
+          style={{ cursor: "pointer" }}
+        />
+      ) : (
+        <SearchIcon className="search-bar__icon" />
+      )}
+    </div>
   );
 };
 
