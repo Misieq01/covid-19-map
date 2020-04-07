33ef5b93a9c2eb1a377f7a5c165e07ccafe1eaa9
Misieq01 <38589417+Misieq01@users.noreply.github.com>
2020-04-07 18:24:10 +0200

    Removed zoom controls from map on mobile + fixed bouncing when zooming out

--- src/containers/Map.js
+++ src/containers/Map.js
@@ -46,18 +46,20 @@ const Map = () => {
       {...mapParametrs}
       minZoom={3}
       maxZoom={6}
+      bounceAtZoomLimits={false}
+      zoomControl={window.innerWidth > 1200}
       //I need to track curently value of zoom to applay right multiplayer for circle size
-      onzoomend={event => setZoom(event.target._zoom)}
+      onzoomend={(event) => setZoom(event.target._zoom)}
       //Red comments inside MapMarker
-      onmousemove={window.innerWidth > 1200 ? event => dispatch(updateCursorPosition(event.latlng)) : null}
+      onmousemove={window.innerWidth > 1200 ? (event) => dispatch(updateCursorPosition(event.latlng)) : null}
       maxBounds={[
         [-85, -200],
-        [85, 200]
+        [85, 200],
       ]}
       maxBoundsViscosity={1}
       inertia={false}
-      onclick={window.innerWidth > 1200 ? null : event => event.target.closePopup()}
-      ondrag={window.innerWidth > 1200 ? null : event => event.target.closePopup()}
+      onclick={window.innerWidth > 1200 ? null : (event) => event.target.closePopup()}
+      ondrag={window.innerWidth > 1200 ? null : (event) => event.target.closePopup()}
     >
       <TileLayer
         url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
