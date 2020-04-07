7200076a77421d93f142acfb1f970cbd07f87494
Misieq01 <38589417+Misieq01@users.noreply.github.com>
2020-04-07 18:23:13 +0200

    :wrench:  Quick fix in data-box reponisive design

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,27 +1,31 @@
 import React from "react";
-import {useSelector,useDispatch} from 'react-redux'
-import {getSortBy} from '../store/selectors'
-import {setContentDisplay} from '../store/actions'
+import { useSelector, useDispatch } from "react-redux";
+import { getSortBy } from "../store/selectors";
+import { setContentDisplay } from "../store/actions";
 
 import { ReactComponent as MapIcon } from "../assets/map.svg";
 import { ReactComponent as PanelIcon } from "../assets/panel.svg";
 
 const Navigation = ({ content }) => {
-  const dispatch = useDispatch()
-  const color = useSelector(state=>getSortBy(state)).color
-  const style = {borderBottom: '6px solid ' + color}
+  const dispatch = useDispatch();
+  const color = useSelector((state) => getSortBy(state)).color;
+  const style = { borderBottom: "6px solid " + color };
 
-  const setContent = (content) =>{
-    dispatch(setContentDisplay(content))
-  }
+  const setContent = (content) => {
+    dispatch(setContentDisplay(content));
+  };
 
   return (
     <div className="navigation__container">
-      <div className="navigation__wrapper" style={content === "panel" ? style : null}>
-        <PanelIcon className="navigation__icon" onClick={() => setContent("panel")} />
+      <div
+        className="navigation__wrapper"
+        style={content === "panel" ? style : null}
+        onClick={() => setContent("panel")}
+      >
+        <PanelIcon className="navigation__icon" />
       </div>
-      <div className="navigation__wrapper" style={content === "map" ? style : null}>
-        <MapIcon className="navigation__icon" onClick={() => setContent("map")} />
+      <div className="navigation__wrapper" style={content === "map" ? style : null} onClick={() => setContent("map")}>
+        <MapIcon className="navigation__icon" />
       </div>
     </div>
   );
