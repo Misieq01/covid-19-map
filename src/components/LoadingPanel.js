8fbb8bbe86ff8b13517a4f994442e13bb920c84e
Misieq01 <38589417+Misieq01@users.noreply.github.com>
2020-04-08 14:10:40 +0200

    Deleted animation from loading screen

--- src/components/LoadingPanel.js
+++ src/components/LoadingPanel.js
@@ -1,78 +1,7 @@
 import React from "react";
-import { motion } from "framer-motion";
 
-const firstCircle = {
-  initial: {
-    height: 0,
-    width: 0,
-    borderRadius: 0,
-  },
-  animate: {
-    height: "150px",
-    width: "150px",
-    borderRadius: "75px",
-  },
-};
-const secondCircle = {
-  initial: {
-    height: "100px",
-    width: "100px",
-    borderRadius: "50px",
-
-  },
-  animate: {
-    height: 0,
-    width: 0,
-    borderRadius: 0,
-
-  },
-};
-const thirdCircle = {
-  initial: {
-    height: "200px",
-    width: "200px",
-    borderRadius: "100px",
-
-  },
-  animate: {
-    height: 0,
-    width: 0,
-    borderRadius: 0,
-
-  },
-};
-
-const transition = { yoyo: Infinity, duration: 1.5,ease: 'easeOut'};
 
 const LoadingPanel = () => {
-  return (
-    <>
-      <motion.div
-        className="loading__circle"
-        style={{ background: "rgb(255, 82, 82)" }}
-        variants={firstCircle}
-        initial="initial"
-        animate="animate"
-        transition={transition}
-      />
-      <motion.div
-        className="loading__circle"
-        style={{ background: "rgb(255, 160, 51)" }}
-        variants={secondCircle}
-        initial="initial"
-        animate="animate"
-        transition={transition}
-      />
-      <motion.div
-        className="loading__circle"
-        style={{ background: "rgb(120, 212, 92)" }}
-        variants={thirdCircle}
-        initial="initial"
-        animate="animate"
-        transition={{...transition}}
-      />
-      <span className="loading__text">Stay tight, we are loading application for you</span>
-    </>
-  );
+  return <span className="loading__text">Stay tight, we are loading application for you</span>
 };
 export default LoadingPanel;
